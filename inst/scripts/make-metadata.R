6afbabcf40ce90c2181e4840de811f19798069a5
Drinchai <drinchai@gmail.com>
2021-03-31 01:39:47 -0700

    updated version

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -4,25 +4,24 @@
 ###
 
 meta <- data.frame(
-  Title = paste0("Microaray expression matrix and clinical data for 67 septicemic",
-                   "patients from GEO"),
+  Title = "GSE13015_GPL6106_Microaray_SEPSIS",
   Description = paste0("Microarray expression data",
-                         "for 67 septicemic patients, represented as an ",
-                         "ExpressionSet. R data representation derived from ",
-                         "GEO accession GSE13015, GPL6106."),
-  BiocVersion = "3.8",
+                       "for 67 septicemic patients, represented as an ",
+                       "SummarizedExperiment R data representation derived from ",
+                       "GEO accession GSE13015, GPL6106."),
+  BiocVersion = "3.13",
   Genome = "hg19",
   SourceType = "tar.gz",
   SourceUrl = "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE13015",
-  SourceVersion = "Jul 1, 2009",
+  SourceVersion = "Jul 1 2009",
   Species = "Homo sapiens",
   TaxonomyId = NA,
   Coordinate_1_based = NA,
   DataProvider = "GEO",
   Maintainer = "Darawan Rinchai <drinchai@gmail.com>",
-  RDataClass = c("ExpressionSet", "SummarizedExperiment"),
+  RDataClass = "SummarizedExperiment",
   DispatchClass = "Rda",
-  RDataPath = "GSE13015_GPL6106/GSE13015_GPL6106.Rda" ,
+  RDataPath = "GSE13015/GSE13015_GPL6106_SummarizedExperiment.Rda" ,
   Tags = "",
   Notes = "Example data for BloodGen3Module Package"
 )
